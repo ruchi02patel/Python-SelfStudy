7d4c6ffe6148c412f8ca004e4ff49807ea7e41e1
ruchi02patel <38772561+ruchi02patel@users.noreply.github.com>
2018-04-30 16:33:30 -0400

    Add files via upload

---
+++ my_module.py
@@ -0,0 +1,19 @@
+# this is the file "my_module.py"
+# i will be using this module for "TryItOut#1Chapter15.py"
+def printruchi():
+    print "RRRRR   U    U    CCCC   H     H  IIIIIII"
+    print "R    R  U    U   C    C  H     H     I"
+    print "R    R  U    U  C        H     H     I"
+    print "RRRRR   U    U  C        HHHHHHH     I"
+    print "R    R   U  U    C    C  H     H     I"
+    print "R    R    UU      CCCC   H     H  IIIIIII" 
+
+def printpatel():
+    print "PPPPP     A     TTTTTTT  EEEEEEE  L"
+    print "P    P   A A       T     E        L"
+    print "P    P  A   A      T     E        L"
+    print "PPPPP  AAAAAAA     T     EEEEE    L"
+    print "P     A       A    T     E        L"
+    print "P    A         A   T     E        L"
+    print "P   A           A  T     EEEEEEE  LLLLLLL"
+
